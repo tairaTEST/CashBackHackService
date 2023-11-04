12e6b7362464dd5f16886f5bd84e359e4af7cef9
Tatiana Noskova <tnoskowa@mail.ru>
2023-11-04 20:13:07 +0500

    test commit

--- src/test/java/ru/netology/unit/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashBackHackServiceTest.java
@@ -1,7 +1,24 @@
 package ru.netology.unit;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashBackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CachBackHackService service = new CachBackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashBack() {
+        CachBackHackService service = new CachBackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
 }
-@Test
